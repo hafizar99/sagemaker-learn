20dd0f8d6c1be1208282473b2fadf968f8c17c01
Hafiz Arraja <hafiz.arraja@zebrax.id>
2019-06-21 10:49:39 +0700

    Lab 01

---
+++ config.js
@@ -0,0 +1,5 @@
+// Initialize the Amazon Cognito credentials provider
+AWS.config.region = 'us-east-1'; // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: 'us-east-1:83026c71-3922-449e-b55e-1393d4574540',
+});
